9bb828c489bee647833c6e0fe064eb229b49217e
gnuh24 <devilskipper@gmail.com>
2023-08-07 20:16:42 +0700

    VTI

---
+++ SQL/Assignment Day 3 - Lesson 3/Extra_Assignment_3/Extra_Assignment_3.sql
@@ -0,0 +1,15 @@
+USE `trainee`;
+
+INSERT INTO `Fresher_management` (TraineeID, Full_Name,  Birth_Date, Gender, ET_IQ, ET_Gmath, ET_English, Training_Class, Evaluation_Notes, VTI_Account)
+VALUES 
+	(130, "NgoTuanHung1", "2004-04-02", "Male", 20, 20, 50, "DKP130", "Good", "ROCKET38-02"),
+    (131, "NgoTuanHung2", "2002-03-10", "Female", 16, 18, 45, "DKP131", "Average", "ROCKET38-13"),
+    (132, "NgoTuanHung3", "2001-07-22", "Male", 15, 17, 43, "DKP132", "Excellent", "ROCKET38-14"),
+    (133, "NgoTuanHung4", "2003-12-05", "Female", 17, 16, 49, "DKP133", "Good", "ROCKET38-15"),
+    (134, "NgoTuanHung5", "2002-06-30", "Male", 19, 15, 48, "DKP134", "Average", "ROCKET38-16"),
+    (135, "NgoTuanHung6", "2001-10-18", "Female", 16, 18, 45, "DKP135", "Excellent", "ROCKET38-17"),
+    (136, "NgoTuanHung7", "2000-08-12", "Male", 17, 19, 47, "DKP136", "Good", "ROCKET38-18"),
+    (137, "NgoTuanHung8", "2003-02-25", "Female", 15, 17, 43, "DKP137", "Average", "ROCKET38-19"),
+    (138, "NgoTuanHung9", "2004-05-08", "Male", 18, 16, 49, "DKP138", "Excellent", "ROCKET38-20"),
+	(139, "NgoTuanHung10", "2000-01-15", "Male", 18, 19, 47, "DKP139", "Good", "ROCKET38-12");
+
