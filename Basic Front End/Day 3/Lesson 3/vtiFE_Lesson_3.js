6aa9389731fa824dbb05a901ee5667cae7e2945c
gnuh24 <devilskipper@gmail.com>
2023-10-04 22:43:42 +0700

    22h43 by THug24

--- Basic Front End/Day 3/Lesson 3/vtiFE_Lesson_3.js
+++ Basic Front End/Day 3/Lesson 3/vtiFE_Lesson_3.js
@@ -1,12 +1,25 @@
-class Person{
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
+// class Person{
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
 
-    sayHello(){
-        console.log("Hello my name's " + this.name + ". I'm " + this.age + " year(s) old!!");
-    }
-}
-var list = ["a", 1, "c"];
+//     sayHello(){
+//         console.log("Hello my name's " + this.name + ". I'm " + this.age + " year(s) old!!");
+//     }
+// }
 
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age;
+
+//     this.speak = function(){
+//         console.log("My name's ${this.name}");
+//     }
+   
+// }
+
+// let person1 = new Person("Suifeng", 19);
+// person1.speak;
+for (let i=0; i<10; i++);
+console.log(i);
