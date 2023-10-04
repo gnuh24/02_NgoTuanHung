a8a9678719083ae72da0c67dbda4732fd50bf58a
gnuh24 <devilskipper@gmail.com>
2023-10-04 19:01:27 +0700

    19h01 by THug24

---
+++ Basic Front End/Day 3/Lesson 3/vtiFE_Lesson_3.js
@@ -0,0 +1,12 @@
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    sayHello(){
+        console.log("Hello my name's " + this.name + ". I'm " + this.age + " year(s) old!!");
+    }
+}
+var list = ["a", 1, "c"];
+
