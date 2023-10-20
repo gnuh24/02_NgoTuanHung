788718ef9f1f7d2215785019ef5411336db5dbdf
gnuh24 <devilskipper@gmail.com>
2023-10-20 22:27:27 +0700

    .

--- Java Advanced/Day 1/TestingSystem_Assignment_1/src/main/java/Demo/Program.java
+++ Java Advanced/Day 1/TestingSystem_Assignment_1/src/main/java/Demo/Program.java
@@ -23,5 +23,6 @@ public class Program {
 
 	
 	public static void main(String[] args) {	
+		
 	}	
 }
---
+++ Java Advanced/Day 3/TestingSystem_Assignment_3/src/main/java/com/vti/TestingSystem_Assignment_3/App.java
@@ -0,0 +1,13 @@
+package com.vti.TestingSystem_Assignment_3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
