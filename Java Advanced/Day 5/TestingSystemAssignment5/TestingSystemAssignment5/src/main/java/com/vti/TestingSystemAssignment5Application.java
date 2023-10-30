a3b6e8a18f2b74a4eb85dfbbf17dee29c0e786cd
gnuh24 <devilskipper@gmail.com>
2023-10-30 15:16:16 +0700

    VTI TSA 5

---
+++ Java Advanced/Day 5/TestingSystemAssignment5/TestingSystemAssignment5/src/main/java/com/vti/TestingSystemAssignment5Application.java
@@ -0,0 +1,15 @@
+package com.vti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSystemAssignment5Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TestingSystemAssignment5Application.class, args);
+
+	}
+
+}
---
+++ Java Advanced/Day 5/TestingSystemAssignment5/TestingSystemAssignment5/src/test/java/com/example/TestingSystemAssignment5/TestingSystemAssignment5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestingSystemAssignment5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSystemAssignment5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
